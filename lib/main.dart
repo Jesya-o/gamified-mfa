e09d8e20d31c1915d40d699b94d079aa058dcf88
Ksenija Okuneva <ksenija.okuneva.ok@gmail.com>
2024-11-17 13:59:13 +0200

    typo

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,f
+    options: DefaultFirebaseOptions.currentPlatform
   );
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(MfaGamificationApp());
