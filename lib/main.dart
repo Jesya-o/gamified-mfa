4191c121b7c346017584d1b89318c8942c7bad99
Ksenija Okuneva <ksenija.okuneva.ok@gmail.com>
2024-11-17 10:18:57 +0200

    firebase background message handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,28 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  if (kDebugMode) {
+    print('Handling a background message: ${message.messageId}');
+  }
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(MfaGamificationApp());
 }
 
 class MfaGamificationApp extends StatelessWidget {
+  const MfaGamificationApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
