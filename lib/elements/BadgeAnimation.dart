561c5b6b444f6c2065c5a32e61bcc1a4f496a86e
Ksenija Okuneva <ksenija.okuneva.ok@gmail.com>
2024-11-22 11:21:45 +0200

    Revert "cooler badge"
    This reverts commit e28538362b535f85bc8d9e52674bb3964d181a04.

--- lib/elements/BadgeAnimation.dart
+++ lib/elements/BadgeAnimation.dart
@@ -1,14 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class BadgeAnimation extends StatefulWidget {
   final String text;
   final Duration duration;
 
-  const BadgeAnimation({
-    super.key,
-    required this.text,
-    this.duration = const Duration(seconds: 2),
-  });
+  const BadgeAnimation({Key? key, required this.text, this.duration = const Duration(seconds: 2)})
+      : super(key: key);
 
   @override
   _BadgeAnimationState createState() => _BadgeAnimationState();
@@ -16,8 +14,7 @@ class BadgeAnimation extends StatefulWidget {
 
 class _BadgeAnimationState extends State<BadgeAnimation> with SingleTickerProviderStateMixin {
   late AnimationController _controller;
-  late Animation<double> _glowAnimation;
-  late Animation<Color?> _colorAnimation;
+  late Animation<double> _opacityAnimation;
 
   @override
   void initState() {
@@ -25,18 +22,9 @@ class _BadgeAnimationState extends State<BadgeAnimation> with SingleTickerProvid
     _controller = AnimationController(
       duration: widget.duration,
       vsync: this,
-    )..repeat(reverse: true);
-
-    _glowAnimation = Tween<double>(begin: 0.5, end: 1.0).animate(
-      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
     );
-
-    _colorAnimation = ColorTween(
-      begin: Colors.blue,
-      end: Colors.purple,
-    ).animate(
-      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
-    );
+    _opacityAnimation = Tween<double>(begin: 1.0, end: 0.0).animate(_controller);
+    _controller.forward();
   }
 
   @override
@@ -48,55 +36,24 @@ class _BadgeAnimationState extends State<BadgeAnimation> with SingleTickerProvid
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
-      animation: _controller,
+      animation: _opacityAnimation,
       builder: (context, child) {
-        return Center(
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              // Gradient background with glow effect
-              Container(
-                width: 120,
-                height: 120,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  gradient: RadialGradient(
-                    colors: [
-                      _colorAnimation.value!,
-                      Colors.transparent,
-                    ],
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                      color: _colorAnimation.value!.withOpacity(0.5),
-                      blurRadius: 20 * _glowAnimation.value,
-                      spreadRadius: 5 * _glowAnimation.value,
-                    ),
-                  ],
-                ),
+        return Positioned(
+          top: 50,
+          left: MediaQuery.of(context).size.width / 2 - 50,
+          child: Opacity(
+            opacity: _opacityAnimation.value,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.circular(10),
               ),
-              // Badge text with custom styling
-              Text(
+              child: Text(
                 widget.text,
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  foreground: Paint()
-                    ..shader = LinearGradient(
-                      colors: [Colors.pink, Colors.orange],
-                    ).createShader(Rect.fromLTWH(0.0, 0.0, 200.0, 70.0)),
-                ),
+                style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
               ),
-              Positioned(
-                top: 10 * _glowAnimation.value,
-                left: 10 * _glowAnimation.value,
-                child: Icon(
-                  Icons.star,
-                  color: Colors.yellow.withOpacity(0.8),
-                  size: 10,
-                ),
-              ),
-            ],
+            ),
           ),
         );
       },
