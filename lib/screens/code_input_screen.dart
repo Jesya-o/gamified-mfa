18c56eb4489fa8ea449e95ddd2647695b4a28659
Ksenija Okuneva <ksenija.okuneva.ok@gmail.com>
2024-11-24 11:43:25 +0200

    numeric keyboard in secret code input

--- lib/screens/code_input_screen.dart
+++ lib/screens/code_input_screen.dart
@@ -4,7 +4,9 @@ import 'package:shared_preferences/shared_preferences.dart';
 import '../elements/badge_animation.dart';
 import '../elements/fade_badge.dart';
 import '../elements/gamification_manager.dart';
+import '../elements/numeric_keypad.dart';
 import '../elements/points_display.dart';
+import '../elements/shake_animation.dart';
 import 'home_screen.dart';
 
 class CodeInputScreen extends StatefulWidget {
@@ -16,8 +18,9 @@ class CodeInputScreen extends StatefulWidget {
 
 class _CodeInputScreenState extends State<CodeInputScreen> {
   final _formKey = GlobalKey<FormState>();
-  String _secretCode = '';
+  String _inputCode = '';
   bool _isGamificationEnabled = true;
+  bool _shouldShake = false;
 
   @override
   void initState() {
@@ -32,59 +35,80 @@ class _CodeInputScreenState extends State<CodeInputScreen> {
     });
   }
 
-  Future<void> _submitCode() async {
-    if (_formKey.currentState!.validate()) {
-      _formKey.currentState!.save();
-
-      bool isCodeCorrect = _secretCode == '123456';
-
-      String message = isCodeCorrect
-          ? "Authentication Successful!"
-          : "Authentication Failed!";
-      Color badgeColor = isCodeCorrect ? Colors.green : Colors.red;
+  Future<void> _proceed() async {
+    if (_isGamificationEnabled) {
+      String message = "Authentication Successful!";
 
       Overlay.of(context)?.insert(
         OverlayEntry(
-          builder: (context) => FadeBadge(message: message, color: badgeColor),
+          builder: (context) => FadeBadge(message: message, color: Colors.lightGreen),
         ),
       );
-
-      if (isCodeCorrect) {
-        int currentPoints = await GamificationManager.getPoints();
-        int currentLevel = await GamificationManager.getLevel();
-
-        int newPoints = currentPoints + 5;
-        if (newPoints >= 50) {
-          currentLevel += 1;
-          newPoints = 0;
-        }
-
-        await GamificationManager.updatePoints(newPoints);
-        await GamificationManager.updateLevel(currentLevel);
-
-        if (_isGamificationEnabled) {
-          Overlay.of(context)?.insert(
-            OverlayEntry(
-              builder: (context) => BadgeAnimation(text: "+5"),
-            ),
-          );
-        }
-      }
-
-      Navigator.pushAndRemoveUntil(
-        context,
-        MaterialPageRoute(builder: (context) => HomeScreen()),
-        (route) => false,
+      
+      Overlay.of(context)?.insert(
+        OverlayEntry(
+          builder: (context) => BadgeAnimation(text: "+5"),
+        ),
       );
     }
+
+    int currentPoints = await GamificationManager.getPoints();
+    int currentLevel = await GamificationManager.getLevel();
+
+    int newPoints = currentPoints + 5;
+    if (newPoints >= 50) {
+      currentLevel += 1;
+      newPoints = 0;
+    }
+
+    await GamificationManager.updatePoints(newPoints);
+    await GamificationManager.updateLevel(currentLevel);
+    
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => HomeScreen()),
+          (route) => false,
+    );
+  }
+
+  Future<void> _submitCode() async {
+    bool isCodeCorrect = _inputCode == '123456';
+    if (isCodeCorrect) {
+      _proceed();
+    } else {
+      setState(() {
+        _shouldShake = true;
+      });
+
+      Future.delayed(const Duration(milliseconds: 500), () {
+        setState(() {
+          _inputCode = '';
+          _shouldShake = false;
+        });
+      });
+    }
+  }
+
+  void _onNumberTap(String number) {
+    setState(() {
+      if (_inputCode.length < 6) {
+        _inputCode += number;
+      }
+    });
+  }
+
+  void _onBackspaceTap() {
+    setState(() {
+      if (_inputCode.isNotEmpty) {
+        _inputCode = _inputCode.substring(0, _inputCode.length - 1);
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Enter Secret Code'),
-        ),
+        appBar: AppBar(),
         body: Stack(
           children: [
             Padding(
@@ -92,27 +116,34 @@ class _CodeInputScreenState extends State<CodeInputScreen> {
               child: Form(
                 key: _formKey,
                 child: Column(
+                  mainAxisSize: MainAxisSize.min,
                   children: [
-                    Text('Please enter your secret code:'),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        labelText: 'Secret Code',
+                    Text(
+                      'Confirm the Request',
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 20),
+                    ShakeAnimation(
+                      shouldShake: _shouldShake,
+                      child: Text(
+                        _inputCode,
+                        style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 2,
+                        ),
                       ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter the code';
-                        }
-                        return null;
-                      },
-                      onSaved: (value) {
-                        _secretCode = value!;
-                      },
+                    ),
+                    SizedBox(height: 20),
+                    NumericKeypad(
+                      onNumberTap: _onNumberTap,
+                      onBackspaceTap: _onBackspaceTap,
                     ),
                     SizedBox(height: 20),
                     ElevatedButton(
-                      onPressed: _submitCode,
-                      child: Text('Submit'),
+                      onPressed: _inputCode.isNotEmpty ? _submitCode : null,
+                      child: Text('Verify'),
                     ),
                   ],
                 ),
